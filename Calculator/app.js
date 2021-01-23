4556bca909bba5855511b8ea1ed819b62bd31842
muhammdnaeemmadni <76472888+muhammdnaeemmadni@users.noreply.github.com>
2021-01-24 00:27:35 +0500

    Add files via upload

---
+++ Calculator/app.js
@@ -0,0 +1,19 @@
+function bu(a){
+    var number = document.getElementById("input")
+    number.value += a
+}
+function c(){
+    var  number = document.getElementById("input")
+    number.value=""
+}
+function but(){
+    var number=document.getElementById("input")
+    number.value =eval(number.value)
+}
+// function d(){
+//     var  number = document.getElementById("input")
+//     number.value=""
+// }
+
+
+
